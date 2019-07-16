5130bce850969d513a242489031d4838b5db6425
Semih Raif Gürel <semih@weptile.com>
2019-07-16 19:22:47 +0300

    ...

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import { connect } from 'react-redux';
@@ -22,13 +21,6 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
 
 				<h2>{  this.props.user }</h2>
 				<button onClick={this.onUpdateUser}>Change the name</button>
